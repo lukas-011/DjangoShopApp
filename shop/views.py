7b4f0736c31579655a454acb8285001e3ca7c51e
Lukas McCain <lukasmccain0@gmail.com>
2024-03-13 23:05:06 -0400

    Made hypertext links for the shop, so you can go to the item in the shop

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,21 @@
 from .models import Course
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
+
 
 def single_course(request, course_id):
-    course = Course.object.get(pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
+    # #OPTION 1
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'shop/single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+
+    # OPTION 2
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {'course': course})
