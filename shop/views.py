c109010d076e4ca58dd7765649d01d5f783101e4
Lukas <lukasmccain0@gmail.com>
2024-03-13 21:03:27 -0400

    Added bootstrap style sheets to the courses.h file to spicen it up

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Course
 from django.shortcuts import render
 
 def index(request):
     courses = Course.objects.all()
     return render(request, 'courses.html', {'courses': courses})
+
+def single_course(request, course_id):
+    course = Course.object.get(pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
